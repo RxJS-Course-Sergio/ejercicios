516dd72838fb38a9f5cdc59ed6fb3a11f092dd35
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-04 14:12:43 -0700

    add diff types of requests with ajax

---
+++ src/ajax/04-post-put-delete.ts
@@ -0,0 +1,45 @@
+import { ajax } from "rxjs/ajax";
+
+const url = "https://httpbin.org/delay/1";
+
+// ajax.post(
+//   url,
+//   {
+//     id: 1,
+//     nombre: "Sergio",
+//   },
+//   {
+//     "mi-token": "abc123",
+//   }
+// ).subscribe(console.log);
+
+// ajax.put(
+//   url,
+//   {
+//     id: 1,
+//     nombre: "Sergio",
+//   },
+//   {
+//     "mi-token": "abc123",
+//   }
+// ).subscribe(console.log);
+
+// ajax.delete(
+//   url,
+//    {
+//     "mi-token": "abc123",
+//   }
+
+// ).subscribe(console.log);
+
+ajax({
+  url,
+  method: "POST",
+  headers: {
+    "mi-token": "abc123",
+  },
+  body: {
+    id: 1,
+    nombre: "Sergio",
+  },
+}).subscribe(console.log);
